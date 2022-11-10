ab1909e36c9fc9c33dc51611723a41cb196951a6
Vedant <116330731+Vedant1033@users.noreply.github.com>
2022-11-10 09:41:55 +0530

    Add files via upload

---
+++ Group 10 Mini Project 1A/CODES/com/mysql/jdbc/Connection.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mysql.jdbc;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author Vedant
+ */
+public class Connection {
+
+    public PreparedStatement preparedStatement(String inert_user_datanameusernameemailgenderspo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public PreparedStatement prepareStatement(String inert_user_datanameusernameemailgenderspo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
